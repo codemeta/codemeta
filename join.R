8b13453ffc15fcd25e71d75e758ffc02c49186b4
Carl Boettiger <cboettig@gmail.com>
2016-04-16 23:27:39 -0700

    use column definitions from Kyle and Neil

--- join.R
+++ join.R
@@ -39,22 +39,31 @@ A <- read_csv("group_a_crosswalk.csv", col_types = paste0(rep("c", 16), collapse
 B <- read_csv("group_b_crosswalk.csv", col_types = paste0(rep("c", 23), collapse=""))
 C <- read_csv("group_c_crosswalk.csv", col_types = paste0(rep("c", 17), collapse=""))
 D <- read_csv("crosswalk-OntoSoft.csv", col_types = paste0(rep("c", 15), collapse=""))
+E <- read_csv("kyle-neil-join.csv") %>% select(Concept, `Concept Definition`)
 
-A <- A %>% select(Concept, `Concept Definition`, Zenodo, `Software Ontology`, GitHub, `Use Cases`)
-B <- B %>% select(-Zenodo, -`Software Ontology`, -github, -`Software Ontology`, -DataCite, -OntoSoft)
-C <- C %>% select(Concept, `Concept Definition`, `Use Cases`, `Software Entities Model (DataCitei/JISC)`) %>% rename("DataCite" = `Software Entities Model (DataCitei/JISC)`)
-D <- D %>% select(Concept, `Concept Definition`, OntoSoft)
+
+A <- A %>% select(Concept, Zenodo, `Software Ontology`, GitHub, `Use Cases`)
+B <- B %>% select(-`Concept Definition`, -Zenodo, -`Software Ontology`, -github, -`Software Ontology`, -DataCite, -OntoSoft)
+C <- C %>% select(Concept, `Use Cases`, `Software Entities Model (DataCitei/JISC)`) %>% rename("DataCite" = `Software Entities Model (DataCitei/JISC)`)
+D <- D %>% select(Concept, OntoSoft)
 
 
 A %>%  
   full_join(B, by = "Concept") %>% 
   full_join(C, by = "Concept") %>% 
-  full_join(D, by = "Concept") -> crosswalk
+  full_join(D, by = "Concept") %>% 
+  full_join(E,by = "Concept") -> crosswalk
 
-is.na(crosswalk) <- "-"
+fillna <- function(df){
+  for(i in 1:length(df)){
+    df[[i]][is.na(df[[i]])] <- "-"
+  }
+  df
+}
 
-
+crosswalk <- fillna(crosswalk)
 ## Fill NA
 ## Remove non-ascii
+
 
 write_csv(crosswalk, "crosswalk.csv")
