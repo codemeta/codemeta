2ed132689f659dbe9722fac7c824a7b48b42c34a
Carl Boettiger <cboettig@gmail.com>
2016-04-16 23:45:45 -0700

    order data frame

--- join.R
+++ join.R
@@ -44,7 +44,7 @@ E <- read_csv("kyle-neil-join.csv") %>% select(Concept, `Concept Definition`)
 
 A <- A %>% select(Concept, Zenodo, `Software Ontology`, GitHub, `Use Cases`)
 B <- B %>% select(-`Concept Definition`, -Zenodo, -`Software Ontology`, -github, -`Software Ontology`, -DataCite, -OntoSoft)
-C <- C %>% select(Concept, `Use Cases`, `Software Entities Model (DataCitei/JISC)`) %>% rename("DataCite" = `Software Entities Model (DataCitei/JISC)`)
+C <- C %>% select(Concept, `Use Cases`, `Software Entities Model (DataCitei/JISC)`) %>% rename(`Software Entities Model (DataCitei/JISC)` = "DataCite")
 D <- D %>% select(Concept, OntoSoft)
 
 
@@ -65,5 +65,31 @@ crosswalk <- fillna(crosswalk)
 ## Fill NA
 ## Remove non-ascii
 
-
+crosswalk <- select(crosswalk, 
+`Concept`,
+  `Concept Definition`,
+  `DataCite`,                                 
+  `OntoSoft`,
+  `Zenodo`,
+  `GitHub`,
+ `Figshare`,
+ `Software Ontology`,
+ `code.jsonld`,                              
+ `Software Discovery Index`,                
+ `Dublin Core`,                             
+ `R Package Description`,                    
+ `Debian Package Metadata`,                 
+ `Python Distutils (PyPI)`,                  
+ `Trove Software Map`,                      
+ `Perl Module Description (CPAN::Meta)`,     
+ `JavaScript package descption (npm)`,
+ `Java (Maven)`,                             
+ `Octave`,                                  
+ `Ruby Gem`,                                 
+ `CodeMeta Field`,                           
+ `Use Cases`,                               
+ `Use Cases.x`,                              
+ `Use Cases.y`)
 write_csv(crosswalk, "crosswalk.csv")
+
+
