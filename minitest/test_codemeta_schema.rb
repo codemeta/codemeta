a98f094b79c155523e8c2c7bea7eeb287b0d5da2
gothub <slaughter@nceas.ucsb.edu>
2016-10-28 11:00:08 -0700

    Verify all examples with JSON Schema

--- minitest/test_codemeta_schema.rb
+++ minitest/test_codemeta_schema.rb
@@ -18,7 +18,43 @@ describe 'example-codemeta-schema' do
     @errorMsgs = JSON::Validator.fully_validate(schema, input)
   end
 
-  it 'CodeMeta instance file validates with CodeMeta JSON schema' do
+  it 'CodeMeta example file validates with CodeMeta JSON schema' do
     assert_empty @errorMsgs, 'example-codemeta-full.json does not validate against CodeMeta JSON schema'
+  end
+end
+
+describe 'example-codemeta-agent' do
+  before do
+    input = JSON.parse(File.read('examples/example-codemeta-agent.json'))
+    schema = JSON.parse(File.read('codemeta-json-schema.json'))
+    @errorMsgs = JSON::Validator.fully_validate(schema, input)
+  end
+
+  it 'CodeMeta exmaple file validates with CodeMeta JSON schema' do
+    assert_empty @errorMsgs, 'example-codemeta-agent.json does not validate against CodeMeta JSON schema'
+  end
+end
+
+describe 'example-codemeta-rdataone' do
+  before do
+    input = JSON.parse(File.read('examples/example-codemeta-rdataone.json'))
+    schema = JSON.parse(File.read('codemeta-json-schema.json'))
+    @errorMsgs = JSON::Validator.fully_validate(schema, input)
+  end
+
+  it 'CodeMeta exmaple file validates with CodeMeta JSON schema' do
+    assert_empty @errorMsgs, 'example-codemeta-rdataone.json does not validate against CodeMeta JSON schema'
+  end
+end
+
+describe 'example-codemeta-full-invalid' do
+  before do
+    input = JSON.parse(File.read('examples/example-codemeta-full-invalid.json'))
+    schema = JSON.parse(File.read('codemeta-json-schema.json'))
+    @errorMsgs = JSON::Validator.fully_validate(schema, input)
+  end
+
+  it 'CodeMeta exmaple file validates with CodeMeta JSON schema' do
+    assert @errorMsgs, 'example-codemeta-full-invalid.json does not generate a validation error for CodeMeta JSON schema (it should!).'
   end
 end
