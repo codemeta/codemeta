8734f8ed80a3fc689eba20271f68c864d454c654
Carl Boettiger <cboettig@gmail.com>
2017-05-12 21:14:20 -0700

    remove website material from docs

--- docs/files/update_cv_pdf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd assets/files
-cp ../../vita.md .
-#pandoc vita.md --template cv-template.tex -f markdown-startnum -o cv.tex
-pandoc vita.md --template cv-template.tex -f markdown-startnum -o cv.pdf
-rm vita.md
-cd
