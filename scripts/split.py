98b8f880282a5c389a232b87d8fb2c264c79ec58
Heinz-Alexander Fuetterer <35225576+afuetterer@users.noreply.github.com>
2026-03-25 14:58:41 +0100

    scripts: Replace os.path with pathlib.Path (#475)

--- scripts/split.py
+++ scripts/split.py
@@ -1,30 +1,21 @@
-#!/usr/bin/env python3
-
-import os
 import csv
-from pprint import pprint
+from pathlib import Path
 
 
-REPO_DIR = os.path.join(os.path.dirname(__file__), '..')
+REPO_DIR = Path(__file__).parent.parent
 
-PROP_DESC_PATH = os.path.join(REPO_DIR, 'properties_description.csv')
+
+PROP_DESC_PATH = REPO_DIR / "properties_description.csv"
 """The CSV file where the leftmost columns of the aggregate crosswalk table
 are (parent type, property name, type, and description."""
 
-SOURCE_DIR = os.path.join(REPO_DIR, 'crosswalks')
+SOURCE_DIR = REPO_DIR / "crosswalks"
 """The directories where all other .csv files are."""
 
-CROSSWALK_PATH = os.path.join(REPO_DIR, 'crosswalk.csv')
+CROSSWALK_PATH = REPO_DIR / "crosswalk.csv"
 """The path/name of the file where the aggregate crosswalk table is
 written."""
 
-
-def rm_file(filename):
-    """Removes a file if it exists, does nothing otherwise."""
-    try:
-        os.unlink(filename)
-    except FileNotFoundError:
-        pass
 
 with open(CROSSWALK_PATH) as fd:
     reader = csv.reader(fd)
@@ -33,14 +24,14 @@ cols = list(zip(*lines))
 props = cols[1]
 for col in cols[4:]:
     col_name = col[0]
-    filename = os.path.join(SOURCE_DIR, col_name + '.csv')
-    rm_file(filename)
+    filename = SOURCE_DIR / f"{col_name}.csv"
+    filename.unlink(missing_ok=True)
     with open(filename, 'a') as fd:
         writer = csv.writer(fd)
         for (prop, trans) in zip(props, col):
             writer.writerow((prop, trans))
 
-rm_file(PROP_DESC_PATH)
+PROP_DESC_PATH.unlink(missing_ok=True)
 with open(PROP_DESC_PATH, 'a') as fd:
     writer = csv.writer(fd, lineterminator='\n')
     prop_desc = zip(*cols[0:4])
